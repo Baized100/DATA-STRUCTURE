ca1160843526956a6be84f2db75f983c6158ea89
Md.Baizid <111190288+Baized100@users.noreply.github.com>
2024-03-22 00:53:36 +0600

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,16 @@
+stack = []
+
+stack.append('Baizid shake')
+stack.append('Shamim shake')
+stack.append('Shakwat shake')
+
+print('Initial stack')
+print(stack)
+
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
